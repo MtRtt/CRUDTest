51a8b9119e0f0130e50f85629cce9ba720dcdb1c
mohrt <mohrt@MOH_RTT>
2024-05-11 01:02:13 +0330

    Add project files.

---
+++ CRUDTest/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using CRUDTest.Data;
+using CRUDTest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDTest.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> CategoryList = _db.Categories;
+            return View(CategoryList);
+        }
+
+        //Get دادن اطلاعات برای نمایش
+        //create view fro this action for give data from user
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //create post action for recive and save data in db 
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //check for model data is correct
+           if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           return View();
+        }
+    }
+}
